3547a7918148f94a432c06314382227cc9eb5e4e
pranavvb03 <139568635+pranavvb03@users.noreply.github.com>
2024-06-28 20:19:45 +0530

    Update weapon_app.py

--- weapon_app.py
+++ weapon_app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv, dotenv_values  # we can use load_dotenv or doten
 load_dotenv()
 
 # print(os.getenv("MY_SECRET_KEY"))
-
+MY_SECRET_KEY = st.secrets['MY_SECRET_KEY']
 genai.configure(api_key=os.getenv("MY_SECRET_KEY"))
 
 # Set up the model
